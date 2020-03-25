0dac278addb8228f83ea5c5852995bb440b1934e
jgibbel <jamesgibbel@gmail.com>
2020-03-24 20:26:45 -0400

    Updates

---
+++ precache-manifest.72ace37bff835744957a9d4680ab930e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62e8d4a33efde85d11f39dd5a06da6b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "684dfc0092965a440266",
+    "url": "/static/css/main.527d3444.chunk.css"
+  },
+  {
+    "revision": "39f573681513fad18794",
+    "url": "/static/js/2.870e127c.chunk.js"
+  },
+  {
+    "revision": "bc23c04eec3c55de9f1e7db1db146add",
+    "url": "/static/js/2.870e127c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "684dfc0092965a440266",
+    "url": "/static/js/main.373dc5c8.chunk.js"
+  },
+  {
+    "revision": "431f04e7b799ff09b1df",
+    "url": "/static/js/runtime-main.5f1f3d92.js"
+  }
+]);
